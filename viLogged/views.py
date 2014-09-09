b6305e32410a72e3d86bf038eba1265b40749c77
musamusa <musakunte@gmail.com>
2014-09-09 14:30:03 +0100

    added auth to pages

--- viLogged/views.py
+++ viLogged/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import login as django_login, logout as django_logout
+from django.contrib.auth.views import login_required, login as django_login, logout as django_logout
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
@@ -8,10 +8,22 @@ class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
 
 
+@login_required
+def visitors_list_page(request):
+    return render(request, 'visitors/index.html', {})
+
+
+@login_required
+def add_visitor(request):
+    return render(request, 'visitors/form.html', {})
+
+
+@login_required
 def login(req, template_name=''):
     return django_login(req, **{"template_name": 'login.html'})
 
 
+@login_required
 def logout(req, template_name=''):
     django_logout(req, **{"template_name": 'login.html'})
     return redirect('/')
