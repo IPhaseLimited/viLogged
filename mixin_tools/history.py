59507ae86d6ff76e373729125f5f6b422676e0be
Musa Musa <musakunte@gmail.com>
2014-11-10 22:11:55 +0100

    changed fields to match client couch name

--- mixin_tools/history.py
+++ mixin_tools/history.py
@@ -1,6 +1,7 @@
 from django.db import models
 from uuidfield import UUIDField
 
+
 class HistoryFieldsMixin(models.Model):
     '''
     Adds common history fields to a model. changed_by field you need to populate yourself.
@@ -9,19 +10,20 @@ class HistoryFieldsMixin(models.Model):
     class Area(HistoryFieldsMixin, models.Model):
         pass
     '''
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    changed_by = models.ForeignKey('auth.User', null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('auth.User', null=True, blank=True)
+    modified_by = models.ForeignKey('auth.User', null=True, blank=True)
     uuid = UUIDField(auto_created=True, unique=True)
 
     # Used by django-simple-history
     @property
     def _history_user(self):
-        return self.changed_by
+        return self.created_by
 
     @_history_user.setter
     def _history_user(self, value):
-        self.changed_by = value
+        self.created_by = value
     # END: Used by django-simple-history
 
     class Meta:
