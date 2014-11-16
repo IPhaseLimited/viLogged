176348d14dcf008efb875b5ff6dfb04315a5eb3e
Musa Musa <musakunte@gmail.com>
2014-11-16 08:34:46 +0100

    removed extra comma

--- viLogged/settings.py
+++ viLogged/settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
     'appointments',
     'staff',
     'grappelli',
-    'django.contrib.admin',,
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
