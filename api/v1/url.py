a1f4f6caf398b39cd9d28822a6e99314d58e4b18
Musa Musa <musakunte@gmail.com>
2014-11-18 20:26:41 +0100

    reaaranged urls

--- api/v1/url.py
+++ api/v1/url.py
@@ -6,10 +6,20 @@ from api.v1.core.views import *
 
 urlpatterns = patterns('',
     url(r'^appointments/nested/?$', AppointmentNestedList.as_view()),
+    url(r'^company-entrance/?$', CompanyEntranceNamesList.as_view()),
+    url(r'^company-entrance/(?P<uuid>\w+)/?$', CompanyEntranceNamesDetail.as_view()),
+    url(r'^company-departments/?$', CompanyDepartmentsList.as_view()),
+    url(r'^company-departments/(?P<uuid>\w+)/?$', CompanyDepartmentsDetail.as_view()),
     url(r'^current-user/?$', GetUserByToken.as_view()),
+    url(r'^restricted-items/?$', RestrictedItemsList.as_view()),
+    url(r'^restricted-items/(?P<uuid>\w+)/?$', RestrictedItemsDetail.as_view()),
+    url(r'^send-email/?$', SendEmail.as_view()),
+    url(r'^vehicles/?$', VehicleList.as_view()),
+    url(r'^vehicles/(?P<uuid>\w+)/?$', VehicleDetail.as_view()),
     url(r'^visitors/nested/?$', VisitorsNestedList.as_view()),
     url(r'^users/?$', UserList.as_view()),
     url(r'^users/nested/?$', UserNestedList.as_view()),
+
     url(r'^appointments/?', include('api.v1.appointments.url')),
     url(r'^core/?', include('api.v1.core.url')),
     url(r'^company-departments/?', include('api.v1.core.url')),
@@ -19,13 +29,6 @@ urlpatterns = patterns('',
     url(r'^visitors-location/(?P<uuid>\w+)/?$', VisitorsLocationDetail.as_view()),
     url(r'^user/?', include('api.v1.user.url')),
 
-    url(r'^company-entrance/?$', CompanyEntranceNamesList.as_view()),
-    url(r'^company-entrance/(?P<uuid>\w+)/?$', CompanyEntranceNamesDetail.as_view()),
-    url(r'^company-departments/?$', CompanyDepartmentsList.as_view()),
-    url(r'^company-departments/(?P<uuid>\w+)/?$', CompanyDepartmentsDetail.as_view()),
-    url(r'^restricted-items/?$', RestrictedItemsList.as_view()),
-    url(r'^restricted-items/(?P<uuid>\w+)/?$', RestrictedItemsDetail.as_view()),
-    url(r'^send-email/?$', SendEmail.as_view()),
-    url(r'^vehicles/?$', VehicleList.as_view()),
-    url(r'^vehicles/(?P<uuid>\w+)/?$', VehicleDetail.as_view()),
+
+
 )
