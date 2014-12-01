fa32977903a0521258e844ad52832c1e363811c7
Musa Musa <musakunte@gmail.com>
2014-12-01 01:25:56 +0100

    removed win db settings

--- viLogged/settings.py
+++ viLogged/settings.py
@@ -16,17 +16,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-# if SYS_OS is 'Windows':
-#     DATABASES = {
-#         'default': {
-#             'NAME': 'viLogged',
-#             'ENGINE': 'sqlserver_ado',
-#             'HOST': 'musa\\SQLEXPRESS',
-#             'USER': '',
-#             'PASSWORD': '',
-#         }
-#     }
-# else:
 
 DATABASES = {
         'default': {
@@ -149,25 +138,10 @@ INSTALLED_APPS = (
     'djoser',
     'django_extensions',
     'core',
-    'appointments',
-    'staff',
-    'grappelli',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
-TWILIO_ACCOUNT_SID = 'AC9bea919627d63c232851c8e56faf6435'
-TWILIO_AUTH_TOKEN = '0f37345fc2c507b4f4ae31c82fdee6ab'
-TWILIO_DEFAULT_CALLERID = 'viLogged VMS'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'ncc@iphtech.com'
-EMAIL_HOST_PASSWORD = '*nccnaija#'
-DEFAULT_FROM_EMAIL = 'ncc@iphtech.com'
 
 
 FIXTURE_DIRS = (
