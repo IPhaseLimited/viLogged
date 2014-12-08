0d289620b01dbcf6057af3be5f690679fd0ca6fc
Musa Musa <musakunte@gmail.com>
2014-12-08 15:01:34 +0100

    added ldap feature and changed file structure

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from api.v1.core.views import AuthUser
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #url(r'^$', HomePageView.as_view()),
+    url(r'^api/', include('api.url')),
+    url(r'^auth-user/?$', AuthUser.as_view()),
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^admin/', include(admin.site.urls)),
+)
